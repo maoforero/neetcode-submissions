58157c8a53c12d614b74f0da7e3b3c5f8329d6e8
maoforero <42775364+maoforero@users.noreply.github.com>
2026-06-13 17:04:29 -0500

    Find the stray number

---
+++ codewars/csharp/7-kyu/find-the-stray-number.cs
@@ -0,0 +1,33 @@
+class Solution 
+{
+  public static int Stray(int[] numbers)
+  {
+    for(int i = 0; i < numbers.Length; i++){
+      var first = numbers[i];
+      var midd  = numbers[i+1];
+      var limit = numbers[i+2];
+      
+      if(first != midd && midd != limit)
+        return midd;
+      
+      if(first == midd && midd != limit)
+        return limit;
+      
+      if(first != midd && midd == limit)
+        return first;
+    }
+    
+    return 0;
+  }
+}
+using NUnit.Framework;
+
+[TestFixture]
+public class SolutionTest
+{
+  [Test]
+  public void SimpleArray1()
+  {
+    Assert.That(Solution.Stray(new int[] {1, 1, 2}), Is.EqualTo(2));
+  }
+}
