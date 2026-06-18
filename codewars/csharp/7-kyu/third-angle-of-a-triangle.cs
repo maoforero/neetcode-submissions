0ef4500a64c2d4ee8f63060c6488063550858350
maoforero <42775364+maoforero@users.noreply.github.com>
2026-06-18 11:27:27 -0500

    Third Angle of a Triangle

---
+++ codewars/csharp/7-kyu/third-angle-of-a-triangle.cs
@@ -0,0 +1,24 @@
+using System;
+
+public static class Kata
+{
+  public static int OtherAngle(int a, int b) => 180 - (a + b); 
+}
+namespace Solution 
+{
+  using NUnit.Framework;
+  using System;
+
+  [TestFixture]
+  public class SolutionTest
+  {
+    [Test]
+    public void SampleTest()
+    {
+      Assert.That(Kata.OtherAngle(30, 60), Is.EqualTo(90));
+      Assert.That(Kata.OtherAngle(60, 60), Is.EqualTo(60));
+      Assert.That(Kata.OtherAngle(43, 78), Is.EqualTo(59));
+      Assert.That(Kata.OtherAngle(10, 20), Is.EqualTo(150));
+    }
+  }
+}
